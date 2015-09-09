75feba076afbdf0a099a6c9892689b834e1ed76e
Jason Grundstad <jgrundstad@gmail.com>
2015-09-09 10:21:47 -0500

    first commit

---
+++ ucsc_tracks.py
@@ -0,0 +1,19 @@
+__author__ = 'jgrundst'
+from flask import Flask, request, render_template
+import os
+templ_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask(__name__, template_folder=templ_dir)
+
+
+@app.route("/")
+def hello():
+    render_template('/echo.html')
+
+
+@app.route("/echo", methods=['POST'])
+def echo():
+    return "You said: " + request.form['text']
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
