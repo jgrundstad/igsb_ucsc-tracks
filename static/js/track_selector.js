0789b3e83353b71a9587958585fa0a6aa019db14
Jason Grundstad <jgrundstad@gmail.com>
2015-10-02 14:49:32 -0500

    filter and linkout buttons aligned properly

--- static/js/track_selector.js
+++ static/js/track_selector.js
@@ -25,3 +25,53 @@ function build_filter(genome) {
         $('#filter_select_container').html(selectObj);
     });
 };
+
+$(document).ready(function() {
+    $('select[name=genome_select]').change(function() {
+        var cboxes = $('#bw_checkboxes');
+        var genome = $(this).val();
+        console.log('Selected genome: ' + genome);
+    });
+
+    if ($('select[name=genome_select]')) {
+        build_filter($('select[name=genome_select]').val());
+        $('')
+    };
+
+    $('#checkAll').click(function() {
+        $(".check").prop('checked', $(this).prop('checked'));
+    });
+
+    $('#genLinkOut').click(function() {
+        console.log("Clicked the linkOut button");
+        var selected = [];
+        $('#bigwigs input:checked').each(function (){
+            selected.push($(this).attr('value'));
+        });
+        trackString = selected.join();
+        console.log(trackString);
+
+        // new link-out button to appear on click
+        selected_genome = $('#genome_select option:selected').text();
+        var linkButton = $('<button/>', {
+            class: 'btn btn-success',
+            text: 'I want to go to there ',
+            click: function () {
+                $.get('/update_trackDB', {
+                    selected_genome: selected_genome,
+                    tracks: trackString
+                }).done(function () {
+                    console.log("Success!");
+                });
+            },
+        }).append($('<span></span>').addClass('fa').addClass('fa-external-link-square'));
+        $('#linkOutDiv').html(linkButton);
+
+
+        <!--var newButt = $('<button class="btn btn-success" type="submit" id="goToLink">'+-->
+                         <!--'<span class="fa fa-external-link-square">&nbspI want to go to there</span>');-->
+        <!--$('#linkOutDiv').html(newButt);-->
+    });
+
+
+});
