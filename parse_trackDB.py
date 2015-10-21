cc3e8f5a555a09a647db552ec71232083c9a2dd7
Jason Grundstad <jgrundstad@gmail.com>
2015-10-21 15:22:20 -0500

    reverted to copy+paste method for adding hub

--- parse_trackDB.py
+++ parse_trackDB.py
@@ -6,8 +6,8 @@ import sys
 __author__ = 'A. Jason Grundstad'
 
 
-def parse_master(genome_dir=None):
-    template_file = open(os.path.join(genome_dir, 'trackDb.txt.master'), 'r')
+def parse_master(track_master=None):
+    template_file = open(track_master, 'r')
     d = collections.OrderedDict()
     sub_d = collections.OrderedDict()
     current_track = ''
@@ -23,29 +23,72 @@ def parse_master(genome_dir=None):
                 d[current_track] = sub_d
             current_track = ''
             sub_d = collections.OrderedDict()
+    template_file.close()
     return d
 
 
-def update_trackdbfile(tracks=None, visible_list=None,
-                       output=None):
+def update_trackdbfiles(tracks=None, visible_list=None,
+                       output=None, genome=None):
     """
     Take in dict of orderedDicts, print to provided output [stdout]
+    Must also update:
+        config['base_dir']/hub.txt
+        config['base_dir']/genome.txt
     :param tracks: orderedDict of orderedDicts
     :param visible_list: track names to be set to 'dense'
     :param output: mode of output, file or stdout (default)
     :return: None
     """
-    outfile = open(output, 'w')
-    visible_list.append('flyTrack1')  # always make flyTrack1 visible
-    visible_list = set(visible_list)  # no redundancy
 
+    outfile = open(output, 'w')
+    outfile_base = output.split(os.path.sep)[-1]
+    outfile_dir = os.path.sep.join(output.split(os.path.sep)[0:-1])
+    print "outfile base: {}".format(outfile_base)
+    print "outfile dir: {}".format(outfile_dir)
+    visible_list += ',flyTrack1'
+    visible_set = set(visible_list.split(','))
+    print "writing list: {}\n to outfile {}".format(visible_set, output)
     for track_name in tracks:
         tracks[track_name]['visibility'] = 'hide'
-        if track_name in visible_list:
+        if track_name in visible_set:
             tracks[track_name]['visibility'] = 'dense'
         for key in tracks[track_name]:
             print >>outfile, "{} {}".format(key, tracks[track_name][key])
         print >>outfile, ''
+    outfile.close()
+
+    hubfile_name = outfile_base + '.hub.txt'
+    genomefile_name = outfile_base + '.genome.txt'
+    print "hubfile_name: {}".format(hubfile_name)
+    print "genomefile_name: {}".format(genomefile_name)
+    print "dir: {}".format(outfile_dir)
+    create_hub_file(dir=outfile_dir, hubfile_name=hubfile_name,
+                    hub_name=outfile_base, genomefile_name=genomefile_name)
+    create_genome_file(dir=outfile_dir, genomefile_name=genomefile_name,
+                       genome=genome, outfile_base=outfile_base)
+
+
+def create_genome_file(dir=None, genomefile_name=None, genome=None,
+                       outfile_base=None):
+    genome_file = open(os.path.join(dir, genomefile_name), 'w')
+    content = '''genome {}
+trackDb {}
+'''
+    print >>genome_file, content.format(genome, outfile_base)
+    genome_file.close()
+
+
+def create_hub_file(dir=None, hubfile_name=None, hub_name=None,
+                    genomefile_name=None):
+    hubfile = open(os.path.join(dir, hubfile_name), 'w')
+    content = '''hub {}
+shortLabel {}
+longLabel Fly-Worm Project signal tracks ({})
+genomesFile {}
+email avictorsen@uchicago.edu
+'''
+    print >>hubfile, content.format(hub_name, hub_name, hub_name,
+                                    genomefile_name)
 
 
 def main():
@@ -53,14 +96,16 @@ def main():
                                      "the given trackDB file")
     parser.add_argument('-g', dest='genome_dir', required=True,
                         help="modERN_TrackHub/(?)")
-    parser.add_argument('-v', dest='visible_list',
+    parser.add_argument('-l', dest='visible_list',
                         help="list of track names to be set to 'dense'.  " +
                         "Comma delimited")
+    parser.add_argument('-o', dest='output_filename', required=True,
+                        help="name of outfile containing track collection.")
     args = parser.parse_args()
 
     tracks = parse_master(args.genome_dir)
-    outfile = os.path.join(args.genome_dir, 'trackDb.txt')
-    update_trackdbfile(tracks=tracks, visible_list=args.visible_list.split(','),
+    outfile = os.path.join(args.genome_dir, args.output_filename)
+    update_trackdbfiles(tracks=tracks, visible_list=args.visible_list.split(','),
                        output=outfile)
 
 
--- ucsc_tracks.py
+++ ucsc_tracks.py
@@ -1,6 +1,16 @@
-from flask import Flask, request, render_template, jsonify
 import os
+
+from flask import Flask, request, render_template, jsonify
+from pyvirtualdisplay import Display
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 import parse_trackDB
+
+config = dict()
+
 __author__ = 'jgrundst'
 template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                             'templates')
@@ -9,60 +19,116 @@ app = Flask(__name__, template_folder=template_dir)
 dm_filter_options = ['W3l', 'Embryo', 'WPP', 'Adult m/f', 'Adult f', 'Adult m']
 ws_filter_options = []
 
+
 @app.route("/")
 def main_page():
     genomes = config['genomes']
     return render_template('select.html', genomes=genomes)
 
+
 @app.route("/show_available_tracks", methods=['POST'])
 def show_available():
     print "called select_available_tracks"
     selected_genome = request.form['genome_select']
-    # print "selected: " + selected_genome
     if selected_genome == '-- select genome --':
         global config
         return render_template('select.html', genomes=config['genomes'])
     else:
-        genome_path = get_bw_files(genome=request.form['genome_select'])
-        tracks = parse_trackDB.parse_master(genome_path)
+        trackdb_master = get_track_master(genome=request.form['genome_select'])
+        tracks = parse_trackDB.parse_master(trackdb_master)
         return render_template('select.html', genomes=config['genomes'],
-                               genome_path=genome_path, genome=selected_genome,
-                               tracks=tracks,)
+                               genome_path=trackdb_master,
+                               genome=selected_genome, tracks=tracks,)
+
 
 @app.route("/update_trackDB", methods=['POST', 'GET'])
 def update_tracks():
+    global config
     print "Called update_tracks"
-    selected_genome = request.form['selected_genome']
-    selected_tracks = request.form['tracks']
+    selected_genome = request.args.get('selected_genome')
+    selected_tracks = request.args.get('tracks')
+    outfile_name = os.path.join(config['base_dir'], selected_genome,
+                                'trackDbs', request.args.get('outfile'))
     print "selected_genome is: {}".format(selected_genome)
     print "selected_tracks: {}".format(selected_tracks)
+    print "provided linkDb name: {}".format(outfile_name)
+
+    master_file = os.path.join(config['base_dir'], selected_genome, 'trackDbs',
+                               'trackDb.txt.master')
+    print "base_dir in config: {}".format(config['base_dir'])
+    master_tracks = parse_trackDB.parse_master(track_master=master_file)
+    parse_trackDB.update_trackdbfiles(tracks=master_tracks,
+                                      visible_list=selected_tracks,
+                                      output=outfile_name,
+                                      genome=selected_genome)
+
+    # add_hub(url=config['hubUrl'], filename=outfile_name)
 
     return render_template('select.html', genomes=config['genomes'])
+
 
 @app.route("/get_filter_options", methods=['GET'])
 def get_filter_options():
     genome = request.args.get('genome')
     if 'dm' in genome:
-        print "yes, dm is in genome"
         return jsonify(filters=dm_filter_options)
     elif 'WS' in genome:
         return jsonify(filters=ws_filter_options)
     else:
         return None
 
-def get_bw_files(genome=None):
+
+@app.route("/get_config", methods=['GET'])
+def get_config():
     global config
-    genome_path = os.path.join(config['base_dir'], genome, 'trackDbs')
-    print genome_path
-    return genome_path
+    return jsonify(config=config)
+
+
+def get_track_master(genome=None):
+    global config
+    trackdb_master = os.path.join(config['base_dir'], genome,
+                                  'trackDbs', 'trackDb.txt.master')
+    print trackdb_master
+    return trackdb_master
+
 
 def parse_config(filename=None):
     d = {'genomes': ['dm3', 'dm6', 'WS220', 'WS235', 'histone'],
-         'base_dir': 'modERN_TrackHub'}
+         'base_dir': 'modERN_TrackHub',
+         'hubUrl':
+             'https://genome.ucsc.edu/cgi-bin/hgHubConnect?hgsid=443214985_l1fwTXq7lMR7Z7W9lliOUu1XHJ4c',
+         'hubFileUrl': 'http://igsbmod.uchicago.edu/~avictorsen/modERN_TrackHub'}
+
     return d
 
 
-if __name__ == "__main__":
+def find_by_xpath(locator=None, driver=None):
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, locator))
+    )
+    return element
+
+
+def add_hub(url=None, filename=None):
+    """
+
+    :param url: location of "add a hub" page
+    :param filename: relative location of new hub file
+    :return:
+    """
     global config
+    d = Display(visible=0, size=(800, 600))
+    d.start()
+    driver = webdriver.Firefox()
+    driver.get(url)
+    find_by_xpath('//input[@name = "hubText"]').send_keys(
+        os.path.join(config['hubFileUrl'], filename)
+    )
+    # find_by_xpath('//input[@name = "hubAddButton"]').click()
+    # d.stop()
+
+
+if __name__ == "__main__":
+    # global config
     config = parse_config(filename='config.json')
     app.run(debug=True)
